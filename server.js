65fc70703902836644d58ab42dc966710a6b787f
Rouslan Placella <rouslan@placella.com>
2016-02-18 21:15:38 +0000

    Redirect http to https

--- server.js
+++ server.js
@@ -3,6 +3,13 @@
 var express = require('express');
 var fs      = require('fs');
 
+function redirectSec(req, res, next) {
+  if (req.headers['x-forwarded-proto'] == 'http') {
+      res.redirect('https://' + req.headers.host + req.path);
+  } else {
+      return next();
+  }
+}
 
 /**
  *  Define the sample application.
@@ -119,7 +126,7 @@ var NotesApp = function() {
 
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
+            self.app.get(r, redirectSec, self.routes[r]);
         }
     };
 
